3bd718860297a1088ca1ee50cd72001198f76c8e
Jan Hrbek <hrbekj@email.cz>
2022-12-23 08:31:31 +0100

    Vytvoreni projektu. Vytvoreni layoutu apky a vytvoreni prvnich prazdnych stranek a navigace

---
+++ src/pages/OnePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    One page
+    <pre>{{ myVar }}</pre>
+    <q-input v-model="myVar"></q-input>
+    <pre>{{ computedEx }}</pre>
+  </q-page>
+</template>
+
+<script>
+// import { computed } from '@vue/reactivity'
+import { computed, ref } from 'vue'
+
+export default {
+  setup () {
+    const myVar = ref('Some Message')
+    const computedEx = computed(() => {
+      return myVar.value + ' dalsi text'
+    })
+
+    return {
+      myVar,
+      computedEx
+    }
+  }
+}
+</script>
